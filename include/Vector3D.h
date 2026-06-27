33e38445b8bcef582f1285cb5f35bde0cae3d8d7
Mayur Ghatage <101817932+CodeCrafterX21@users.noreply.github.com>
2026-06-27 23:53:54 +0530

    Add full Vector3D operator set: sub, div, dot, magnitude, normalize

--- include/Vector3D.h
+++ include/Vector3D.h
@@ -1,5 +1,6 @@
 #ifndef AETHER_SIM_VECTOR3D_H
 #define AETHER_SIM_VECTOR3D_H
+#include <cmath>
 
 namespace Aether {
 
@@ -23,6 +24,27 @@ struct Vector3D {
 
     Vector3D operator*(double scalar) const {
         return Vector3D(x * scalar, y * scalar, z * scalar);
+    }
+
+    Vector3D operator-(const Vector3D& other) const {
+    return Vector3D(x - other.x, y - other.y, z - other.z);
+    }
+
+    Vector3D operator/(double scalar) const {
+    return Vector3D(x / scalar, y / scalar, z / scalar);
+    }
+
+    double dot(const Vector3D& other) const {
+    return x * other.x + y * other.y + z * other.z;
+    }
+
+    double magnitude() const {
+    return std::sqrt(x * x + y * y + z * z);
+    }
+
+    Vector3D normalize() const {
+        double mag = magnitude();
+        return Vector3D(x / mag, y / mag, z / mag);
     }
 };
 
